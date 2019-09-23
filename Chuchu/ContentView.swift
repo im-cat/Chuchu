55c3f7a7733ae988842f2ca27a8ca1e2167017b6
mohok <pnujhseo@pusan.ac.kr>
2019-09-23 19:24:41 +0900

    Initial Commit

---
+++ Chuchu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chuchu
+//
+//  Created by 정화 on 23/09/2019.
+//  Copyright © 2019 mohomogu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
